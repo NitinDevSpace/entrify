0fc4bd0120e5e90ea8e0d65634c429c17fde8df5
Nitin <nitindevspace@gmail.com>
2025-07-06 21:59:30 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold underline text-center'>Welcome to Entrify</h1>
+    </>
+  )
+}
+
+export default App
