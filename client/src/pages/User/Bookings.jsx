4725e23f23294553ebe269d8a99839cb4218588e
Nitin <nitindevspace@gmail.com>
2025-07-13 18:40:25 +0530

    bookings redesign

--- client/src/pages/User/Bookings.jsx
+++ client/src/pages/User/Bookings.jsx
@@ -38,7 +38,10 @@ const Bookings = () => {
 						return (
 							<Col key={booking._id} xs={{ span: 24 }} lg={{ span: 12 }}>
 								<Card className="mb-3">
-									<div className="d-flex flex-column-mob">
+									<h3 className="text-center text-2xl font-bold">
+										{booking.show.movie.title}
+									</h3>
+									<div className="flex">
 										<div className="flex-shrink-0">
 											<img
 												src={booking.show.movie.poster}
@@ -47,7 +50,6 @@ const Bookings = () => {
 											/>
 										</div>
 										<div className="show-details flex-1">
-											<h3 className="mt-0 mb-0">{booking.show.movie.title}</h3>
 											<p>
 												Theatre: <b>{booking.show.theatre.name}</b>
 											</p>
@@ -55,14 +57,17 @@ const Bookings = () => {
 												Seats: <b>{booking.seats.join(", ")}</b>
 											</p>
 											<p>
-												Date & Time:
+												Date:{" "}
+												<b>{moment(booking.show.date).format("MMM Do YYYY")}</b>
+											</p>
+											<p>
+												Time:{" "}
 												<b>
-													{moment(booking.show.date).format("MMM Do YYYY")}
 													{moment(booking.show.time, "HH:mm").format("hh:mm A")}
 												</b>
 											</p>
 											<p>
-												Amount:
+												Amount:{" "}
 												<b>
 													Rs.{booking.seats.length * booking.show.ticketPrice}
 												</b>
